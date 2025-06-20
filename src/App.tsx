9e29ca3af617f8030b70dd05ce5dbb81de23b754
ritul <ritul>
2025-06-20 18:49:14 +0530

    test: husky and lint-staged working check

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => {
       <div>
         <Router>
           <Suspense
-            fallback={<div className="p-4 text-center">Loading...</div>}
+            fallback={<div className="p-4 text-center">Page Loading...</div>}
           />
           <Routes>
             <Route path="/" element={<Root />} />
