62dc36983fcc809f8127a567097928800bbe7d23
Parth Dhemeliya <parthd@mindfiresolutions.com>
2025-07-11 09:32:12 +0530

    Fix ESLint config and add Next.js ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default tseslint.config(
     ignores: ["dist", ".next", "node_modules", "out", "build", "coverage"],
   },
   {
-    extends: ["next", js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
